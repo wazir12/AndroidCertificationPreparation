24d2b3a858c7ff893195d10d3399fa683fb1ccd0
Seema Chhiber <simchu@Seemas-MacBook-Pro.local>
2020-10-11 00:31:43 +0530

    Adding Recycler View

--- app/src/main/java/com/lwazir/aad/notetakingapp/MainActivity.java
+++ app/src/main/java/com/lwazir/aad/notetakingapp/MainActivity.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayAdapter<NoteInfo> mAdapterNotes;
+    private  NoteRecyclerAdapter recyclerAdapter;
+
+    //private ArrayAdapter<NoteInfo> mAdapterNotes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,24 +42,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mAdapterNotes.notifyDataSetChanged();
+        recyclerAdapter.notifyDataSetChanged();
+      //  mAdapterNotes.notifyDataSetChanged();
     }
     private void initializeDisplayContent() {
-        final ListView listNotes = (ListView) findViewById(R.id.list_notes);
+        //final ListView listNotes = (ListView) findViewById(R.id.list_notes);
 
-        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+        //List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        //mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
 
-        listNotes.setAdapter(mAdapterNotes);
+        //listNotes.setAdapter(mAdapterNotes);
 
-        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+        //listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          //  @Override
+           // public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+             //   Intent intent = new Intent(MainActivity.this, NoteActivity.class);
 //                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-                intent.putExtra(NoteActivity.NOTE_POSITION, position);
-                startActivity(intent);
-            }
-        });
+               // intent.putExtra(NoteActivity.NOTE_POSITION, position);
+                //startActivity(intent);
+           // }
+        //});
+
+        final RecyclerView recycler_notes = (RecyclerView) findViewById(R.id.list_notes);
+        LinearLayoutManager notesLinearLayoutManager = new LinearLayoutManager(this);
+        recycler_notes.setLayoutManager(notesLinearLayoutManager);
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        recyclerAdapter =new NoteRecyclerAdapter(this, notes);
+        recycler_notes.setAdapter(recyclerAdapter);
+
+
     }
 }
---
+++ app/src/main/java/com/lwazir/aad/notetakingapp/NoteRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.lwazir.aad.notetakingapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ConcurrentModificationException;
+import java.util.List;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.CustomViewHolder> {
+    private final LayoutInflater layoutInflator;
+    private final Context mContext;
+    List<NoteInfo> mNotes;
+    public NoteRecyclerAdapter(Context context, List<NoteInfo> notes) {
+        mContext = context;
+        layoutInflator = LayoutInflater.from(context);
+        mNotes = notes;
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
+        View view = layoutInflator.inflate(R.layout.item_note_list, parent , false);
+        return new CustomViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder customViewHolder, @SuppressLint("RecyclerView") int i) {
+            NoteInfo note =  mNotes.get(i);
+            customViewHolder.courseTextView.setText(note.getCourse().getTitle());
+            customViewHolder.titleTxtView.setText(note.getTitle());
+            customViewHolder.mCurrentPosition = i;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView courseTextView;
+        public final TextView titleTxtView;
+        public int mCurrentPosition;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            courseTextView = (TextView)  itemView.findViewById(R.id.text_course);
+            titleTxtView = (TextView) itemView.findViewById(R.id.text_title);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(mContext,NoteActivity.class);
+                    intent.putExtra(NoteActivity.NOTE_POSITION,mCurrentPosition);
+                    mContext.startActivity(intent);
+                }
+            });
+        }
+    }
+}
