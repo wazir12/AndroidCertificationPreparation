c3aaec9f8fee9984b328ac0c4dc8beb78ee3f6d0
Seema Chhiber <simchu@Seemas-MacBook-Pro.local>
2020-10-11 04:03:31 +0530

    Adding Navigation Drawer

--- app/src/main/java/com/lwazir/aad/notetakingapp/MainActivity.java
+++ app/src/main/java/com/lwazir/aad/notetakingapp/MainActivity.java
@@ -4,11 +4,17 @@ package com.lwazir.aad.notetakingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,7 +23,8 @@ import android.widget.ListView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+implements NavigationView.OnNavigationItemSelectedListener {
     private  NoteRecyclerAdapter recyclerAdapter;
 
     //private ArrayAdapter<NoteInfo> mAdapterNotes;
@@ -32,10 +39,20 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, NoteActivity.class));
             }
         });
+        //Initialize Navigation Drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         initializeDisplayContent();
     }
@@ -71,5 +88,34 @@ public class MainActivity extends AppCompatActivity {
         recycler_notes.setAdapter(recyclerAdapter);
 
 
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_notes) {
+           /// displayNotes();
+        } else if (id == R.id.nav_courses) {
+            //displayCourses();
+        } else if (id == R.id.nav_share) {
+           // handleSelection("Don't you think you've shared enough");
+        } else if (id == R.id.nav_send) {
+            //handleSelection("Send");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
