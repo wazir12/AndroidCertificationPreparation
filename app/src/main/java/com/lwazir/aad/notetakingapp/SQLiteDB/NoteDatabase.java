77f68124e58d9f7395c5f7b1572094ca87f54803
Seema Chhiber <simchu@Seemas-MacBook-Pro.local>
2020-10-13 21:06:28 +0530

    Adding PRIMARY KEY and Implementing BASE COLUMN Interface

--- app/src/main/java/com/lwazir/aad/notetakingapp/SQLiteDB/NoteDatabase.java
+++ app/src/main/java/com/lwazir/aad/notetakingapp/SQLiteDB/NoteDatabase.java
@@ -1,15 +1,18 @@
 package com.lwazir.aad.notetakingapp.SQLiteDB;
 
+import android.provider.BaseColumns;
+
 public final class NoteDatabase {
     private NoteDatabase() {
     }
-    public static final class CourseInfoEntry{
+    public static final class CourseInfoEntry implements BaseColumns {
         public static final String  TABLE_NAME = "course_info";
         public static final String  COLUMN_COURSE_ID = "course_id";
         public static final String  COLUMN_COURSE_TITLE = "course_title";
 
         public static final String  CREATE_TABLE_QUERY =
                 "CREATE TABLE "+TABLE_NAME+"( "
+                        +_ID+" INTEGER PRIMARY KEY, "
                 +COLUMN_COURSE_ID+" TEXT UNIQUE NOT NULL"+", "
                         +COLUMN_COURSE_TITLE+" TEXT NOT NULL"+")";
 
@@ -18,14 +21,15 @@ public final class NoteDatabase {
 
     }
 
-    public static final class NoteInfoEntry{
+    public static final class NoteInfoEntry implements BaseColumns{
         public static final String TABLE_NAME = "note_info";
         public static final String COLUMN_NOTE_ID = "note_id";
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
 
         public static final String CREATE_TABLE_QUERY =
-                "CREATE TABLE "+TABLE_NAME+"( "
+                "CREATE TABLE "+TABLE_NAME+"( "+
+                        _ID+"INTEGER PRIMARY KEY, "
                         +COLUMN_NOTE_ID+" TEXT  NOT NULL"+", "
                         +COLUMN_NOTE_TITLE+" TEXT NOT NULL"+", "
                         +COLUMN_NOTE_TEXT+ " TEXT"
